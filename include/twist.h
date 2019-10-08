99090c265f3b989d050884dc4e9c2210befada5c
Dominic Abbott <register@dominicabbott.dev>
2019-10-07 19:15:46 -0600

    remove const modifiers from struct Twist data members

--- include/twist.h
+++ include/twist.h
@@ -9,28 +9,28 @@ namespace cube {
 	//specifies a twist made to a cube
 	struct Twist {
 		//specifies the rotation of the twist - either 90 or -90 degrees
-		const int degrees;
+		int degrees;
 		
 		//specifies the side of the cube to be rotated
-		const Face face;
+		Face face;
 
 		//the layer, or 'slice' of the cube to rotate. Layers are numbered starting at zero, the outermost face,
 		//and moving inwards
-		const int layer;
+		int layer;
 
 		//if wide_turn is true, all layers less than or equal to the aforementioned 'layer' variable are rotated
 		//simultaneously. Otherwise, only the layer specified by the 'layer' variable is rotated
-		const bool wide_turn;
+		bool wide_turn;
 
 		//specifies a rotation on the outermost layer of the given face
-		Twist(const int degrees, const Face& face) : face(face), degrees(degrees), layer(0), wide_turn(false) {
+		Twist(const int degrees, const Face face) : face(face), degrees(degrees), layer(0), wide_turn(false) {
 			assert(degrees == 90 || degrees == -90 && "Degrees parameter must be 90 or -90");
 		}
 		
 		//specifies a given rotation on the layer specified by the 'face' and 'layer' paramaters. If wide_turn is true,
 		//all layers less than and equal to the provided layer paramater are rotated. Otherwise, only the layer specified by
 		//the layer attribute are rotated
-		Twist(const int degrees, const Face& face, const int layer, const bool wide_turn=true) : 
+		Twist(const int degrees, const Face face, const int layer, const bool wide_turn=true) : 
 			wide_turn(wide_turn), face(face), degrees(degrees), layer(layer) {
 				assert(degrees == 90 || degrees == -90 && "Degrees parameter must be 90 or -90");
 			}
