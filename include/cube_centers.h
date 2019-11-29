a1505d708115af4b972309dad3f077261093638a
Dominic Abbott <register@dominicabbott.dev>
2019-11-29 09:01:15 -0700

    improve interface for retrieving the coordinates of faces and the width of the centers in class CubeCenters

--- include/cube_centers.h
+++ include/cube_centers.h
@@ -63,7 +63,9 @@ namespace cube {
 			//getters for the state of the cube
 			int get_center_pos(const Coords coords) const;
 			int get_center_pos(const int center) const {return centers[center];}
-			std::array<int, 2> get_face_coords(const Face face) const {return face_coords.at(face);};
+			int get_fixed_face_coord(const Face face) const {return face_coords.at(face)[0];};
+			int get_fixed_face_coord_value(const Face face) const {return face_coords.at(face)[1];};
+			int get_center_width() const {return edge_width+1;}
 
 			//performs a rotation on the cube
 			void rotate(const Twist& twist);
