9f78aa334c8f619019af29c0f57dab3fd8121ca0
Dominic Abbott <register@dominicabbott.dev>
2019-12-25 13:37:21 -0700

    add std::hash specialization for class Cube

--- include/hash.h
+++ include/hash.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <array>
 #include "cube_centers.h"
+#include "cube.h"
 #include "twist.h"
 
 namespace std {
@@ -36,13 +37,32 @@ namespace std {
 	template<>
 	struct hash<cube::CubeCenters> {
 		size_t operator()(const cube::CubeCenters& centers) const {
-			size_t seed;
+			size_t seed = 0;
 			for (int i = 0; i < std::pow(centers.get_size()-2, 2)*6; i++) {
 				boost::hash_combine(seed, centers.get_center_pos(i));	
 			}
 
 			return seed;
 		}	
+	};
+	
+	template <>
+	struct hash<cube::Cube> {
+		size_t operator()(const cube::Cube& cube) const {
+			size_t seed = 0;
+			int edge_count = 12*(cube.get_size()-2);
+			int corner_count = 8;
+			for (int i = 0; i < edge_count; i++) {
+				boost::hash_combine(seed, cube.get_edge_pos(i));	
+				boost::hash_combine(seed, cube.get_edge_orientation(i));	
+			}
+			for (int i = 0; i < corner_count; i++) {
+				boost::hash_combine(seed, cube.get_corner_pos(i));	
+				boost::hash_combine(seed, cube.get_corner_orientation(i));	
+			}
+
+			return seed;
+		}
 	};
 }
 
