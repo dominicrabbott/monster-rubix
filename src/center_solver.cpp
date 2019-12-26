e1d6fdffb4bbbb4a55a29eb156703ed6321efa35
Dominic Abbott <register@dominicabbott.dev>
2019-12-26 09:25:25 -0700

    improve status messages in class CenterSolver

--- src/center_solver.cpp
+++ src/center_solver.cpp
@@ -110,22 +110,23 @@ int CenterSolver::count_solved_pieces(const cube::CubeCenters& centers) {
 
 void CenterSolver::solve(const cube::CubeCenters& root_state) {
 	cube::CubeCenters curr_state(root_state);
-
 	int states_searched = 0;
 	int strategy_change_threshold = curr_state.get_size()*2500;
 	auto strategy_1_finished = [strategy_change_threshold, states_searched=0] (const cube::CubeCenters& centers) mutable {
 		states_searched++;
 		return states_searched == strategy_change_threshold;
 	};
+	std::cout << "Beginning solving the centers using strategy 1\n";
 	auto strategy_1_twists = search::best_first_search<cube::CubeCenters, CenterHeuristic>(curr_state, generate_strategy_1(curr_state), strategy_1_finished);
 	notify_listeners(strategy_1_twists);
 	for (const auto& twist : strategy_1_twists) {
 		curr_state.rotate(twist);	
 	}
-	std::cout << "Strategy 1 finished. Starting strategy 2\n";
+	std::cout << "Strategy 1 finished. Beginning commutator based search\n";
 	auto strategy_2_finished = [this](const cube::CubeCenters& centers) {
 		return this->count_solved_pieces(centers) == this->pieces_per_center(centers)*6;
 	};
 	notify_listeners(search::best_first_search<cube::CubeCenters, CenterHeuristic>(curr_state, generate_strategy_2(curr_state), strategy_2_finished));
-	std::cout << "Done solving centers"  << std::endl;
+
+	std::cout << "Finished solving centers\n";
 }
